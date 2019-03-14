e6ce57d0dc869fe33fadc636b506ef0059608ed2
Emanuele Paolini <emanuele.paolini@gmail.com>
2019-03-14 09:03:21 +0100

    added express-handlebar to enable serverless-side templating

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,37 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+// const firebaseUser = require('./firebaseUser');
+
+var hbs = exphbs.create({
+    defaultLayout: 'main',
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        raw: function (options) {return options.fn(this);},
+        foo: function () { return 'FOO!'; },
+        bar: function () { return 'BAR!'; }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// app.use(firebaseUser.validateFirebaseIdToken);
+
+app.get('/', (req, res) => {
+//  console.log('Signed-in user:', req.user);
+  return res.render('home', {
+  //  user: req.user,
+  });
+});
+
+// This HTTPS endpoint can only be accessed by your Firebase Users.
+// Requests need to be authorized by providing an `Authorization` HTTP header
+// with value `Bearer <Firebase ID Token>`.
+exports.app = functions.https.onRequest(app);
+
 exports.noteList = functions.region('europe-west1')
     .https.onRequest((req, res) => {
   db = admin.firestore();
