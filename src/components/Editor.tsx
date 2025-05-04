1cb035d3706da11d82d3717422373ebfee24fd4e
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-05-04 22:52:06 +0200

    installato graphql ...e creato ApolloContainer

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,18 @@
 "use client"
 import React, { useState } from 'react'
-import ReactQuill from 'react-quill-new'
 import 'react-quill-new/dist/quill.snow.css'
+import dynamic from 'next/dynamic';
+
+const ReactQuill = dynamic(() => import('react-quill-new'), {
+  ssr: false, // Indica di non renderizzare questo componente lato server
+});
 
 export default function Editor() {
-      const [value, setValue] = useState('')
-    
-      return <ReactQuill theme="snow" value={value} onChange={setValue} />
+  const [content, setContent] = useState('');
+
+  const handleChange = (value: string) => {
+    setContent(value);
+  };
+
+  return <ReactQuill value={content} onChange={handleChange} />;
 }
---
+++ src/lib/apolloClient.ts
@@ -0,0 +1,9 @@
+// lib/apolloClient.ts
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+export default client;
