7ba370d3bae56fe27161db8e9cee77500c864215
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-05-21 08:22:06 +0200

    avanzamento?

--- src/components/MyQuill.tsx
+++ src/components/MyQuill.tsx
@@ -7,9 +7,9 @@ const config = {
 
 const content = new Delta()
     .insert("Hello world ")
-    .insert({ formula: "e^{i\\pi} + 1 = 0" } )
+    .insert({ formula: "e^{i\\pi} + 1 = 0" })
     .insert(". Taylor series:\n")
-    .insert({ formula: "f(x) = \\sum_{n=0}^{\\infty} \\frac{f^{(n)}(a)}{n!} (x-a)^n" })
+    .insert({ formula: "f(x) = \\sum_{n=0}^{\\infty} \\frac{f^{(n)}(a)}{n!} (x-a)^n" }, {displaystyle: true})
 
 export default function MyQuill() {
     return <>
--- src/lib/myquill.js
+++ src/lib/myquill.js
@@ -1,15 +1,86 @@
 import MyQuill from 'quill-next'
 import MyQuillEditor from 'quill-next-react'
+import { TextBlot, EmbedBlot } from 'parchment';
 
 export { Delta } from 'quill-next'
 
-const Formula = MyQuill.import('formats/formula');
+const GUARD_TEXT = '\uFEFF';
 
-class MyFormula extends Formula {
+class Embed extends EmbedBlot {
+  constructor(scroll, node) {
+    super(scroll, node);
+    this.contentNode = document.createElement('span');
+    this.contentNode.setAttribute('contenteditable', 'false');
+    Array.from(this.domNode.childNodes).forEach(childNode => {
+      this.contentNode.appendChild(childNode);
+    });
+    this.leftGuard = document.createTextNode(GUARD_TEXT);
+    this.rightGuard = document.createTextNode(GUARD_TEXT);
+    this.domNode.appendChild(this.leftGuard);
+    this.domNode.appendChild(this.contentNode);
+    this.domNode.appendChild(this.rightGuard);
+  }
+  index(node, offset) {
+    if (node === this.leftGuard) return 0;
+    if (node === this.rightGuard) return 1;
+    return super.index(node, offset);
+  }
+  restore(node) {
+    let range = null;
+    let textNode;
+    const text = node.data.split(GUARD_TEXT).join('');
+    if (node === this.leftGuard) {
+      if (this.prev instanceof TextBlot) {
+        const prevLength = this.prev.length();
+        this.prev.insertAt(prevLength, text);
+        range = {
+          startNode: this.prev.domNode,
+          startOffset: prevLength + text.length
+        };
+      } else {
+        textNode = document.createTextNode(text);
+        this.parent.insertBefore(this.scroll.create(textNode), this);
+        range = {
+          startNode: textNode,
+          startOffset: text.length
+        };
+      }
+    } else if (node === this.rightGuard) {
+      if (this.next instanceof TextBlot) {
+        this.next.insertAt(0, text);
+        range = {
+          startNode: this.next.domNode,
+          startOffset: text.length
+        };
+      } else {
+        textNode = document.createTextNode(text);
+        this.parent.insertBefore(this.scroll.create(textNode), this.next);
+        range = {
+          startNode: textNode,
+          startOffset: text.length
+        };
+      }
+    }
+    node.data = GUARD_TEXT;
+    return range;
+  }
+  update(mutations, context) {
+    console.log('Embed update', mutations, context);
+    mutations.forEach(mutation => {
+      if (mutation.type === 'characterData' && (mutation.target === this.leftGuard || mutation.target === this.rightGuard)) {
+        const range = this.restore(mutation.target);
+        if (range) context.range = range;
+      }
+    });
+  }
+}
+
+class MyFormula extends Embed {
   static blotName = 'formula'; // importante: deve mantenere lo stesso nome
   static tagName = 'SPAN';     // o ciò che serve
 
   static create(value) {
+    console.log('Formula.create', value);
     // @ts-expect-error
     if (window.katex == null) {
       throw new Error('Formula module requires KaTeX.');
@@ -25,11 +96,34 @@ class MyFormula extends Formula {
     }
     return node;
   }
+
+  format(name, value) {
+    if (name === 'displaystyle') {
+      if (value) {
+        this.domNode.classList.add('tex-displaystyle')
+      } else {
+        this.domNode.classList.remove('tex-displaystyle')
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+
   static value(domNode) {
     return domNode.getAttribute('data-value');
   }
-  
+
+  update(mutations, context) {
+    console.log('MyFormula update', mutations, context);
+    super.update(mutations, context);
+
+    const formula = this.domNode.getAttribute('data-value');
+    const displaystyle = this.domNode.classList.contains('tex-displaystyle');
+    //katex.render(formula, this.domNode, { displayMode: displaystyle });
+  }
+
   html() {
+    console.log('MyFormula html');
     const {
       formula
     } = this.value();
